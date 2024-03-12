6492fa458da4bcc6f02f4cc465332fd4a71db88c
Toustsik Mikahil <toustsikmikhail@gmail.com>
2024-03-12 16:59:34 +0100

    feat: add layout for header

--- good-bona-app/src/app/components/header/header.component.ts
+++ good-bona-app/src/app/components/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderNavBarComponent } from './componets/header-nav-bar/header-nav-bar.component';
+import { LogoComponent } from './componets/logo/logo.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [LogoComponent, HeaderNavBarComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
