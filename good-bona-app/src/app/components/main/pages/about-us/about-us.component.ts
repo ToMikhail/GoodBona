48a88a430c9ac31c2b333360f2f42798b85e258f
Toustsik Mikahil <toustsikmikhail@gmail.com>
2024-04-25 08:07:25 +0200

    feat: add design

--- good-bona-app/src/app/components/main/pages/about-us/about-us.component.ts
+++ good-bona-app/src/app/components/main/pages/about-us/about-us.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
+  public cafeName: string = 'GoodBona '
 
 }
