9e6a30294b6446eee83bc935ddbba4195d492bd7
Toustsik Mikahil <toustsikmikhail@gmail.com>
2024-05-13 14:29:07 +0200

    feat: media quereis for about page

--- good-bona-app/src/app/components/main/pages/about-us/about-us.component.ts
+++ good-bona-app/src/app/components/main/pages/about-us/about-us.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [CommonModule, RouterModule, RouterLink, ButtonComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
